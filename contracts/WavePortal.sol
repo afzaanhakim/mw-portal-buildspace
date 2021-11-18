26a116cbd2f34adb5cf0c7c6cca6af35ceca236e
afzaanhakim <afzaanhakim19@gmail.com>
2021-11-18 16:44:14 -0500

    adding bassic wave function to incrememnt rotal wave count and show address of person calling wave function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,14 @@ import "hardhat/console.sol";
 
 contract WavePortal {
 
+uint256 totalWaves; //delcaring totalWaves to keep count of waves - state variable
   constructor() {
     console.log("smart contract");
   }
+
+function wave() public {
+  totalWaves += 1; //incrementing waves whenevr we recieve one
+  console.log("%s has waved!", msg.sender); //msg.sender = wallet address of persin calling the wave function  
+}
+
 }
